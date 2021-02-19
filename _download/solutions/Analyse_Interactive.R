a375d9ffeab111e0c78219421fc328fc64dc0906
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2021-02-19 19:37:09 +0100

    ..

--- _download/solutions/Analyse_Interactive.R
+++ _download/solutions/Analyse_Interactive.R
@@ -1,43 +1,53 @@
 ### R Skript zu "Analyse"
 ### Kurs "Einführung in die moderne Datenanalyse mit R"
-### Datum: Februar 2020
+### Datum: Februar 2021
 ### Autor: The R Bootcamp
 
 
 ### Einfache Statistiken ----------------------------
 
-# Lese den Datei Tourismus.csv als `daten` ein.
-daten <- read_csv("1_Data/Tourismus.csv")
+# Lese die Datei Tourismus.csv als `daten` ein.
 
-# Berechne den Mittelwert (mean()) von den Variablen `Besucher` und `Dauer`.
+daten <- read.csv('1_Data/Tourismus.csv')
+
+# Berechne den Mittelwert (mean()) der Variablen `Besucher` und `Dauer`.
+
 mean(daten$Besucher)
 mean(daten$Dauer)
 
-# Berechne den Median (median()) von den Variablen `Besucher` und `Dauer`.
+# Berechne den Median (median()) der Variablen `Besucher` und `Dauer`.
+
 median(daten$Besucher)
 median(daten$Dauer)
 
-# Berechne die Standardabweichung (sd()) von den Variablen `Besucher` und `Dauer`.
+# Berechne die Standardabweichung (sd()) der Variablen `Besucher` und `Dauer`.
+
 sd(daten$Besucher)
 sd(daten$Dauer)
 
 # Berechne die Korrelation (cor()) zwischen den Variablen `Besucher` und `Dauer`.
+
 cor(daten$Besucher, daten$Dauer)
 
 ### Einfache Graphiken ----------------------------
 
 # Plotte ein Histogram (hist()) für die Variable `Besucher`.
+
 hist(daten$Besucher)
 
 # Plotte ein Histogram (hist()) für die Variable `Dauer`.
+
 hist(daten$Dauer)
 
 # Plotte ein Streudiagram (plot()) für die Variablen `Dauer` und `Besucher`.
-plot(daten$Dauer, daten$Besucher)
+
+plot(daten$Besucher, daten$Dauer)
 
 # Ändere die Achsen des Streudiagrams zu log-Skalen.
-plot(daten$Dauer, daten$Besucher, log = "x")
 
-# Plotte ein Boxplot für Besucher als Funktion von `Region`.
+plot(daten$Besucher, daten$Dauer, log = "xy")
+
+# Plotte ein Boxplot für `Besucher` als Funktion von `Region`.
+
 boxplot(daten$Besucher ~ daten$Region)
 
--- _download/solutions/DataIO_Interactive.R
+++ _download/solutions/DataIO_Interactive.R
@@ -1,40 +1,49 @@
 ### R Skript zu "Data I/O"
 ### Kurs "Einführung in die moderne Datenanalyse mit R"
-### Datum: Februar 2020
+### Datum: Februar 2021
 ### Autor: The R Bootcamp
 
 
 ### Daten von Festplatte lesen ----------------------------
 
-# Finde die Datei Tourismus.csv auf deinem Computer.
+# Stelle sicher, dass Tourismus.csv in deinem 1_Data Ordner liegt.
 
 # Lese die Datei mittels read.csv() ein. Denke an den Auto-Complete Trick!
-read.csv("1_Data/Tourismus.csv")
 
-# Stelle sicher, dass die Daten im Objekt mit Namen `daten` gespeichert sind.
-daten <- read_csv("1_Data/Tourismus.csv")
+read.csv('1_Data/Tourismus.csv')
+
+# Weise die Daten einem Objekt mit Namen `daten` zu.
+
+daten <- read.csv('1_Data/Tourismus.csv')
 
 ### Daten leben in data.frames ----------------------------
 
 # Überprüfe die Klasse von `daten` mittels class()
+
 class(daten)
 
 # Überprüfe die Dimensionen von `daten` mittels dim()
+
 dim(daten)
 
 # Lass dir die Namen der Variablen in `daten` anzeigen mittels names()
+
 names(daten)
 
 # Extrahiere die Variable `Land` mittels $
+
 daten$Land
 
 # Kreiere ein neues Objekt, dass die Variable `Land` enthält.
-land <- daten$Land
 
-# Erstelle eine neue Variable names 'Naechte' mit $ als das Produkt von Dauer und Besucher.
+Land <- daten$Land
+
+# Erstelle eine neue Variable names `Naechte` mit $ als das Produkt von `Dauer` und `Besucher`.
+
 daten$Naechte <- daten$Dauer * daten$Besucher
 
 ### Daten auf die Festplatte schreiben ----------------------------
 
 # Schreibe den Datensatz Tourismus zurück auf die Festplatte mit write.csv()
-write.csv(daten, "1_Data/Tourismus_neu.csv")
+
+write.csv(daten, '1_Data/Tourismus.csv')
